a6a279d9b4770fa5aa33815ba0a286737e63571d
Jiwoong-Jung <jjw981119@naver.com>
2024-04-05 14:45:12 +0900

    first commit

---
+++ src/views/DataBinding.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Hello, {{ title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "안녕하세요!",
+    };
+  },
+};
+</script>
